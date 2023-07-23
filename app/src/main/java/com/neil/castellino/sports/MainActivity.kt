ed5a300a111692a83e80ca61bd2ffca5c54f7250
Neil <neilcastellino@gmail.com>
2023-07-23 13:43:43 -0700

    Added Data binding, new app logo

--- app/src/main/java/com/neil/castellino/sports/MainActivity.kt
+++ app/src/main/java/com/neil/castellino/sports/MainActivity.kt
@@ -2,16 +2,17 @@ package com.neil.castellino.sports
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.neil.castellino.sports.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.navigation_news -> showFragment(NewsFragment())
                 R.id.navigation_scores -> showFragment(ScoresFragment())
